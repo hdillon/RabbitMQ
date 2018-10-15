a47a39f7fadd314571e2b5c8cbb8f3bd9a97e38e
hdillon <dillonhoraciodavid@gmail.com>
2018-10-15 17:05:15 -0300

    Basic consumer and producer example

---
+++ Consumer/Recieve.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Recieve
+    {
+        static void Main(string[] args)
+        {
+            string hostName = System.Configuration.ConfigurationManager.AppSettings["RabbitMQHostName"];
+            string port = System.Configuration.ConfigurationManager.AppSettings["RabbitMQPort"];
+            var factory = new ConnectionFactory() { HostName = hostName, Port = int.Parse(port) };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "MyQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "MyQueue",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/Send.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            string hostName = System.Configuration.ConfigurationManager.AppSettings["RabbitMQHostName"];
+            string port = System.Configuration.ConfigurationManager.AppSettings["RabbitMQPort"];
+            var factory = new ConnectionFactory() { HostName = hostName, Port = int.Parse(port)};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "MyQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Custom message";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "MyQueue",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
